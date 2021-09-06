ba485be9e8141a8895ac188e13c36ba2709d3d8b
Javier Trombetta <jitrombetta@hotmail.com>
2021-09-06 20:24:39 -0300

    Heroku

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express'); //requiero el módulo exrpess en contante express
 const path = require('path'); //requiero el módulo path en constante path
 
+const PORT = process.env.PORT || 3000;
+
 const publicFolderPath = path.resolve(__dirname, './public'); //resuleve la ruta de carpeta public, en equipo local, y la asigna a publicFolderPath
+
 
 const app = express(); //se asigna la función express a la constante app
 app.use(express.static(publicFolderPath)); //se utiliza la ruta declarada en publicFolderPath para publicar los archivos en el servidor
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Started at http://localhost:3000'); //el servidor escucha en el puerto 3000
 });
 
